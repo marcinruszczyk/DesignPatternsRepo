e761639888042e8d2c37450f20a7b191c5368633
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-02 23:54:18 +0200

    Siingleton

---
+++ ConsoleApp1/Implementation.cs
@@ -0,0 +1,44 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        // Lazy<T>
+        private static readonly Lazy<Logger> _lazyLogger
+             = new Lazy<Logger>(() => new Logger());
+
+        // private static Logger? _instance;
+
+        //Instance
+
+        public static Logger? Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+                //if (_instance == null)
+                //{
+                //    _instance = new Logger();
+                //}
+                //return _instance;
+            }
+        }
+
+
+
+
+        protected Logger()
+        {
+
+
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+
+
+    }
+
+
+}
